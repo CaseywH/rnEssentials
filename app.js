dd9cc96020372fe61c3d6f6716c3ce1e9d2cb3b5
Casey Heinemeyer <doulos1129@gmail.com>
2017-11-13 15:21:15 -0800

    initial deploy

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/loginapp');
-var db = mongoose.connection;
+
+
+var promise = mongoose.connect('mongodb://caseyrn:caseyrn@ds259305.mlab.com:59305/rnessentials', {
+  useMongoClient: true,
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
